91d8a63a0ef4744d3623ba6028b4cc507481764e
papiando (HOME) <info@papiando.com>
2018-01-06 01:10:13 -0400

    Add/edit user roles

--- controller/userrole.controller.php
+++ controller/userrole.controller.php
@@ -4,7 +4,7 @@ namespace Cubo;
 defined('__CUBO__') || new \Exception("No use starting a class without an include");
 
 class UserRoleController extends Controller {
-	private $list_columns = "`id`,`name`,`description`,`title`";
+	private $list_columns = "`id`,`name`,`description`,`status`,`title`";
 	
 	// Admin view: list
 	public function admin_list($columns = "*",$filter = "1",$order = "`title`") {
--- view/userrole/admin/list.php
+++ view/userrole/admin/list.php
@@ -1,14 +1,19 @@
 <?php
 defined('__CUBO__') || new \Exception("No use starting this code without an include");
-
+$root = true;
 $controller = Cubo\Application::getRouter()->getController();
-?><h1>Contact Groups</h1>
+?><h1>User Roles</h1>
 <form id="filter-form" class="form">
 	<div class="form-row d-flex justify-content-between">
-		<div class="col-12">
+		<div class="col-6">
 			<?php
 				$filter = array('id'=>'filter-text','label'=>'Search','prefix'=>'','value'=>'');
 				include($this->_sharedPath.'filter-text.php'); ?>
+		</div>
+		<div class="col-6">
+			<?php
+				$filter = array('id'=>'filter-status','label'=>'Status','prefix'=>'','value'=>STATUS_PUBLISHED);
+				include($this->_sharedPath.'filter-status.php'); ?>
 		</div>
 	</div>
 </form>
@@ -17,6 +22,7 @@ $controller = Cubo\Application::getRouter()->getController();
 	<thead>
 		<tr>
 			<td class="align-middle"><strong>Title</strong></td>
+			<td class="align-middle"><strong>Status</strong></td>
 			<td class="text-right align-middle">
 				<a href="/admin/<?php echo $controller; ?>?action=add"><button class="btn btn-sm btn-success"><i class="fa fa-plus fa-fw"></i></button></a>
 			</td>
@@ -27,6 +33,7 @@ $controller = Cubo\Application::getRouter()->getController();
 foreach($this->_data as $item) {
 ?>		<tr class="table-item d-none" data-item="<?php echo htmlentities(json_encode($item)); ?>" data-filter="none">
 			<td class="align-middle"><?php echo $item->title; ?></td>
+			<td class="align-middle"><?php include($this->_sharedPath.'show-status.php'); ?></td>
 			<td class="text-right align-middle">
 				<a href="/admin/<?php echo $controller; ?>?action=edit&id=<?php echo $item->id; ?>"><button class="btn btn-sm btn-primary"><i class="fa fa-pencil fa-fw"></i></button></a>
 				<a href="/admin/<?php echo $controller; ?>?action=delete&id=<?php echo $item->id; ?>"><button class="btn btn-sm btn-danger"><i class="fa fa-trash fa-fw"></i></button></a>
@@ -36,107 +43,4 @@ foreach($this->_data as $item) {
 }
 ?>	</tbody>
 </table>
-<script>
-$(document).ready(function() {
-	$('#filter-text').on('change paste keyup',function() {
-		var pattern = new RegExp($(this).val().normalize('NFD'),'i');
-		var count = 0;
-		var total = 0;
-		$('.table-item').each(function() {
-			var filter = parseInt($(this).attr('data-filter'));
-			if(pattern.test($(this).data('item').title.normalize('NFD'))) {
-				filter = filter & ~1;
-			} else if(pattern.test($(this).data('item').description.normalize('NFD'))) {
-				filter = filter & ~1;
-			} else if(pattern.test($(this).data('item').tags.normalize('NFD'))) {
-				filter = filter & ~1;
-			} else if(pattern.test($(this).data('item').name.normalize('NFD'))) {
-				filter = filter & ~1;
-			} else {
-				filter = filter | 1;
-			}
-			$(this).attr('data-filter',filter);
-			if(filter) {
-				$(this).addClass('d-none');
-			} else {
-				$(this).removeClass('d-none');
-				count++;
-			}
-			total++;
-		});
-		$('#filter-info').html('Shown '+count+' out of '+total);
-	});
-	$('#filter-status').on('change',function() {
-		var status = parseInt($(this).val());
-		var count = 0;
-		var total = 0;
-		$('.table-item').each(function() {
-			var filter = parseInt($(this).attr('data-filter'));
-			if(status == 0 || status == $(this).data('item').status) {
-				filter = filter & ~2;
-			} else {
-				filter = filter | 2;
-			}
-			$(this).attr('data-filter',filter);
-			if(filter) {
-				$(this).addClass('d-none');
-			} else {
-				$(this).removeClass('d-none');
-				count++;
-			}
-			total++;
-		});
-		$('#filter-info').html('Shown '+count+' out of '+total);
-	});
-	$('#filter-role').on('change',function() {
-		var role = parseInt($(this).val());
-		var count = 0;
-		var total = 0;
-		$('.table-item').each(function() {
-			var filter = parseInt($(this).attr('data-filter'));
-			if(role == 0 || role == $(this).data('item').role) {
-				filter = filter & ~4;
-			} else {
-				filter = filter | 4;
-			}
-			$(this).attr('data-filter',filter);
-			if(filter) {
-				$(this).addClass('d-none');
-			} else {
-				$(this).removeClass('d-none');
-				count++;
-			}
-			total++;
-		});
-		$('#filter-info').html('Shown '+count+' out of '+total);
-	});
-	$('#filter-language').on('change',function() {
-		var language = parseInt($(this).val());
-		var count = 0;
-		var total = 0;
-		$('.table-item').each(function() {
-			var filter = parseInt($(this).attr('data-filter'));
-			if(language == 0 || language == $(this).data('item').language) {
-				filter = filter & ~8;
-			} else {
-				filter = filter | 8;
-			}
-			$(this).attr('data-filter',filter);
-			if(filter) {
-				$(this).addClass('d-none');
-			} else {
-				$(this).removeClass('d-none');
-				count++;
-			}
-			total++;
-		});
-		$('#filter-info').html('Shown '+count+' out of '+total);
-	});
-	$('.table-item').each(function() {
-		$('#filter-text').trigger('change');
-		//$('#filter-status').trigger('change');
-		//$('#filter-role').trigger('change');
-		//$('#filter-language').trigger('change');
-	});
-});
-</script>
+<script src="/view/shared/js/filtering.js"></script>
